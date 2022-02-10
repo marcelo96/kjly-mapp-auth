8203d39401718b54d6284cff625d81116c7340c4
marcelo96 <marceloriosrodriguez@gmail.com>
2022-02-09 18:10:15 -0600

    get envs (otp_app)

--- lib/kjly_mapp_auth/repo.ex
+++ lib/kjly_mapp_auth/repo.ex
@@ -1,5 +1,7 @@
 defmodule KjlyMappAuth.Repo do
+  otp = Application.get_env(:kjly_mapp_auth, :otp_app)
+  IO.inspect otp, label: "gen_env otp_app"
   use Ecto.Repo,
-    otp_app: :kjly_mapp_auth,
+    otp_app: otp,
     adapter: Ecto.Adapters.Postgres
 end
